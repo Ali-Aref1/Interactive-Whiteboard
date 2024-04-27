159a4cf53c49e45ccec87c84ccfe742a1b2e67c5
Ali-Aref1 <ali14915@gmail.com>
2024-04-28 01:55:31 +0300

    works locally

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,8 +1,72 @@
-import './style.css'
-export const Board = () => {
-  return (
-    <div className="board">
-      
-    </div>
-  )
+import React, { useRef, useEffect, useState } from 'react';
+import './style.css';
+
+export interface BoardProps {
+  color: string;
+  tool: string;
+  size: number;
 }
+
+export const Board: React.FC<BoardProps> = ({ color, tool, size }) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [isDrawing, setIsDrawing] = useState(false);
+  
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const context = canvas.getContext('2d');
+    if (!context) return;
+
+    const handleMouseDown = (event: MouseEvent) => {
+      const { offsetX, offsetY } = event;
+      setIsDrawing(true);
+      context.beginPath(); // Start a new path
+      context.moveTo(offsetX, offsetY); // Move to the starting point
+      context.lineWidth = size; // Set the stroke size based on the 'size' prop
+    };
+    const handleMouseMove = (event: MouseEvent) => {
+      if (!isDrawing) return;
+      const { offsetX, offsetY } = event;
+      if (tool === "brush") {
+        context.lineTo(offsetX, offsetY); // Draw line to the current mouse position
+        context.strokeStyle = color; // Set the stroke color
+        context.lineWidth = size; // Set the stroke size based on the 'size' prop
+        context.stroke();
+      } else if (tool === "eraser") {
+        context.clearRect(offsetX, offsetY, size, size); // Erase at mouse position
+      }
+      const root: any = this;
+      if (root) {
+        if(root.timeout) clearTimeout(root.timeout);
+        root.timeout = setTimeout(() => {
+          var base64ImageData = canvas.toDataURL("image/png");
+        }, 1000);
+      }
+    };
+
+    const handleMouseUp = () => {
+      setIsDrawing(false);
+    };
+
+    canvas.addEventListener('mousedown', handleMouseDown);
+    canvas.addEventListener('mousemove', handleMouseMove);
+    canvas.addEventListener('mouseup', handleMouseUp);
+
+    return () => {
+      canvas.removeEventListener('mousedown', handleMouseDown);
+      canvas.removeEventListener('mousemove', handleMouseMove);
+      canvas.removeEventListener('mouseup', handleMouseUp);
+    };
+  }, [isDrawing, color]);
+
+  return (
+    <canvas
+      ref={canvasRef}
+      className="board"
+      width={1720} // Set your desired width
+      height={800} // Set your desired height
+    ></canvas>
+  );
+};
--- src/components/theme-switcher/ThemeSwitcher.tsx
+++ src/components/theme-switcher/ThemeSwitcher.tsx
@@ -6,20 +6,29 @@ export const ThemeSwitcher = () => {
     if(colorMode=='dark'){
       document.documentElement.style.setProperty('--theme-switcher-bg','#0d1016');
       document.documentElement.style.setProperty('--theme-switcher-circle','#1c2330');
+      document.documentElement.style.setProperty('--tool-selected','#10141d');
+      document.documentElement.style.setProperty('--filter','invert(100%)');
     }
     else{
       document.documentElement.style.setProperty('--theme-switcher-bg','#a1a1a1');
-      document.documentElement.style.setProperty('--theme-switcher-circle','#ffffff');
+        document.documentElement.style.setProperty('--theme-switcher-circle','#ffffff');
+        document.documentElement.style.setProperty('--tool-selected','#686868');
+        document.documentElement.style.setProperty('--filter','none');
     }
     const toggleColorMode = () => {
       setColorMode(colorMode == 'light' ? 'dark' : 'light')
       if(colorMode=='light'){
         document.documentElement.style.setProperty('--theme-switcher-bg','#0d1016');
         document.documentElement.style.setProperty('--theme-switcher-circle','#1c2330');
+        document.documentElement.style.setProperty('--tool-selected','#10141d');
+        document.documentElement.style.setProperty('--filter','invert(100%)');
+
       }
       else{
         document.documentElement.style.setProperty('--theme-switcher-bg','#a1a1a1');
         document.documentElement.style.setProperty('--theme-switcher-circle','#ffffff');
+        document.documentElement.style.setProperty('--tool-selected','#686868');
+        document.documentElement.style.setProperty('--filter','none');
       }
       
     }
