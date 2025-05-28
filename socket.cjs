654cc4a33ac8cd196ad035d0bc6a2d23566a4d52
Ali-Aref1 <ali14915@gmail.com>
2025-05-28 04:02:27 +0300

    potential fix

--- socket.cjs
+++ socket.cjs
@@ -60,7 +60,7 @@ function setupSocket(server) {
             const out = {
                 id: socket.id,
                 mouse: data,
-                userId: user ? user.userId : -1,
+                userId: user ? user.id : -1,
                 username: user ? user.username : "User",
                 email: user ? user.email : ""
             };
